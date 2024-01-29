efc604e4e91964f7ee2ce1944b59256fe296e2f8
sarfrajabbasi <sarfrajk96811@gmail.com>
2024-01-29 18:48:07 +0530

    array  methods

--- Array methods/JS/findLastIndex.js
+++ Array methods/JS/findLastIndex.js
@@ -1,4 +1,14 @@
-const array1 = [5, 12, 50, 130, 44];
+
+/*
+findLastIndex:--
+
+ The findLastIndex() method of Array instances iterates the array in reverse order and returns the index of the first element that satisfies the provided testing function. If no elements satisfy the testing function, -1 is returned.
+
+findLastIndex(callbackFn(el,idx,arr), thisArg)
+
+
+ */
+
 
 function isPrime(element) {
   if (element % 2 === 0 || element < 2) {
