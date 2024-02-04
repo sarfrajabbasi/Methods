b080f14f4628c859423a563852f976adb394dd2b
sarfrajabbasi <sarfrajk96811@gmail.com>
2024-02-04 11:38:38 +0530

    array methods

---
+++ Array methods/JS/from.js
@@ -0,0 +1,10 @@
+/* 
+
+# Array.from():--
+The Array.from() static method creates a new, shallow-copied Array instance from an iterable or array-like object.
+
+## Syntax:-- Array.from(arrayLike, mapFn, thisArg)
+
+## Return value :-  A new Array instance.
+
+*/
