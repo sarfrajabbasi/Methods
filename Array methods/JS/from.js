74ad8cd8147104b3c1cd737872d6032d4f2f46f3
sarfrajabbasi <sarfrajk96811@gmail.com>
2024-02-08 13:23:01 +0530

    Array methods

--- Array methods/JS/from.js
+++ Array methods/JS/from.js
@@ -1,21 +1,68 @@
-/* 
+/*  
 
-# Array.from():--
-The Array.from() static method creates a new, shallow-copied Array instance from an iterable or array-like object.
 
-## Syntax:-- Array.from(arrayLike, mapFn, thisArg)
+# Array.from() Method Overview:
 
-## Return value :-  A new Array instance.
+## Functionality: Array.from() creates a new array by copying values from an iterable or array-like object.
 
+## Syntax:
+
+```js
+// javascript
+Array.from(arrayLike)
+Array.from(arrayLike, mapFn)
+Array.from(arrayLike, mapFn, thisArg)
+
+```
+## Parameters:
+
+   - arrayLike: An iterable or array-like object to convert into an array.
+
+   - mapFn (optional): A function applied to each element before adding to the new array.
+
+   - thisArg (optional): Value used as this when executing mapFn.
+
+   - Return Value: A new array instance.
+
+
+
+## Description:
+
+Array.from() accepts iterable objects (like Map and Set) or array-like objects (with a length property and indexed elements).
+It never creates a sparse array; missing index properties become undefined.
+The optional mapFn parameter executes a function on each element similar to map().
+Important for typed arrays to avoid truncation of values during conversion.
 */
 
 
-const foo = Array.from('foo');
+// Examples:-- Array from different sources
 
+// string
+
+const foo = Array.from('foo')//["f", "o", "o"]
 console.log(foo);
 
-const num = Array.from([1,2,3],(x)=>{
-    x*x
-});
+// set
 
-console.log(num);
+const set  = Array.from(new Set(["foo","bar","baz"]))// ["foo", "bar", "baz"]
+
+console.log(set);
+
+// Map
+
+const map = Array.from(new Map([[1,2],[2,4],[4,8]]))//[[1,2],[2,4],[4,8]]
+
+console.log(map);
+
+
+const mapper = new Map([
+   ["1", "a"],
+   ["2", "b"],
+ ]);
+
+ console.log(Array.from(mapper.values()));
+ console.log(Array.from(mapper.keys()));
+
+
+ 
+//  Array from a NodeList
