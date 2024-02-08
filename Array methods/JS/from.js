25f8941cfa556d64da9f3a7e84fbf7f863cd979e
sarfrajabbasi <sarfrajk96811@gmail.com>
2024-02-08 13:43:21 +0530

    Array methods

--- Array methods/JS/from.js
+++ Array methods/JS/from.js
@@ -34,65 +34,78 @@ The optional mapFn parameter executes a function on each element similar to map(
 Important for typed arrays to avoid truncation of values during conversion.
 */
 
-
 // Examples:-- Array from different sources
 
 // string
 
-const foo = Array.from('foo')//["f", "o", "o"]
+const foo = Array.from("foo"); //["f", "o", "o"]
 console.log(foo);
 
 // set
 
-const set  = Array.from(new Set(["foo","bar","baz"]))// ["foo", "bar", "baz"]
+const set = Array.from(new Set(["foo", "bar", "baz"])); // ["foo", "bar", "baz"]
 
 console.log(set);
 
 // Map
 
-const map = Array.from(new Map([[1,2],[2,4],[4,8]]))//[[1,2],[2,4],[4,8]]
+const map = Array.from(
+  new Map([
+    [1, 2],
+    [2, 4],
+    [4, 8],
+  ])
+); //[[1,2],[2,4],[4,8]]
 
 console.log(map);
 
-
 const mapper = new Map([
-   ["1", "a"],
-   ["2", "b"],
- ]);
+  ["1", "a"],
+  ["2", "b"],
+]);
 
- console.log(Array.from(mapper.values()));
- console.log(Array.from(mapper.keys()));
-
-
+console.log(Array.from(mapper.values()));
+console.log(Array.from(mapper.keys()));
 
 //  Array from a NodeList
 // // Create an array based on a property of DOM Elements
 
-const images = document.querySelectorAll('img');
-const sources = Array.from(images,image=> image.src);
+const images = document.querySelectorAll("img");
+const sources = Array.from(images, (image) => image.src);
 console.log(sources);
-const insecureSources = sources.filter((link)=> link.startsWith('http://'));
+const insecureSources = sources.filter((link) => link.startsWith("http://"));
 console.log(insecureSources);
-
 
 // Array from an Array-like object (arguments)
 
-function f(){
-   return Array.from(arguments)
+function f() {
+  return Array.from(arguments);
 }
 
-console.log(f(1,2,3,4,5,6,6));
+console.log(f(1, 2, 3, 4, 5, 6, 6));
 
 // Using arrow functions and Array.from()
 // Using an arrow function as the map function to
 
-console.log(Array.from([1,2,3],x=>x+x));
-
+console.log(Array.from([1, 2, 3], (x) => x + x));
 
 // Generate a sequence of numbers
 // Since the array is initialized with `undefined` on each position,
 // the value of `v` below will be `undefined`
-console.log(Array.from({length:5},(v,i)=>i));
+console.log(Array.from({ length: 5 }, (v, i) => i));
 // [0, 1, 2, 3, 4]
 
+// Sequence generator (range)
 
+const range = (start, stop, step) => {
+  return Array.from(
+    { length: stop - start / step + 1 },
+    (_, i) => start + i * step
+  );
+};
+
+// Generate numbers range 0..4
+
+console.log(range(1, 4, 0));
+
+// Generate numbers range 1..10 with step of 2
